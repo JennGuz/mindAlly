722a7367b26163c92e3415391bb0f109fe336ca1
Jenniffer Guzman <jenniffer.guzman122@gmail.com>
2024-12-13 12:01:08 -0500

    testing buildpacks

--- main.py
+++ main.py
@@ -20,6 +20,5 @@ sio_asgi_app = socketio.ASGIApp(socketio_server = sio, other_asgi_app = app)
 app.add_route("/socket.io/", route=sio_asgi_app, methods=['GET', 'POST'])
 app.add_websocket_route("/socket.io/", route=sio_asgi_app)
 
-
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
